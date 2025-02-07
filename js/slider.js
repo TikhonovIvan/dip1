f6aa923ae7a68e75b87c289740848f28f0ed4ef8
Ivan Tikhonov (Developer) <ivntik77@gmail.com>
2025-02-07 11:03:09 +0600

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".default-carousel", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
