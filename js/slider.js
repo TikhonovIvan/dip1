b2a4f43c849c3a20aa99dc0f9e910c2e7caf1007
Ivan Tikhonov (Developer) <ivntik77@gmail.com>
2025-02-08 10:48:51 +0600

    commit 11

--- js/slider.js
+++ js/slider.js
@@ -18,3 +18,17 @@ let swiper = new Swiper(".default-carousel", {
 //     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 //     plugins: [forms],
 // };
+
+document.querySelectorAll('.custom-arrows').forEach(arrowContainer => {
+    const input = arrowContainer.previousElementSibling;
+
+    arrowContainer.addEventListener('click', event => {
+        const rect = arrowContainer.getBoundingClientRect();
+        const clickY = event.clientY - rect.top;
+        if (clickY < rect.height / 2) {
+            input.stepUp(); // Увеличиваем значение
+        } else {
+            input.stepDown(); // Уменьшаем значение
+        }
+    });
+});
